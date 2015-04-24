ac65e0090341d0eb558376479cec10959334df08
Chris Vickery <chrisinajar@gmail.com>
2015-04-24 16:30:02 -0400

    Plugin file, i like that format...

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-window.jQuery = window.$ = require('jquery');
-window.Backbone = require('backbone');
-
-var _ = require('lodash');
-
-var _require = require;
-
-window.require = function(name) {
-	switch (name) {
-		case "backbone":
-			return Backbone;
-		case "underscore":
-			return _;
-		case "jquery":
-			return $;
-		default:
-			throw new Error("Module shim does not contain " + name);
-	}
-};
-
-require('discus');
-
-require('bootstrap');
+require('./plugins');
 
 var App = require('./app');
 
---
+++ js/plugins.js
@@ -0,0 +1,29 @@
+// global
+window.jQuery = window.$ = require('jquery');
+window.Backbone = require('backbone');
+Backbone.$ = $;
+
+// local
+var _ = require('lodash');
+
+// Fuck you require, I don't know why it fails to find these modules for discus, but it does
+// so we define this to catch the modules it fails to load..
+// this MUST be before discus is included..
+window.require = function(name) {
+	switch (name) {
+		case "backbone":
+			return Backbone;
+		case "underscore":
+			return _;
+		case "jquery":
+			return $;
+		default:
+			throw new Error("Module shim does not contain " + name);
+	}
+};
+
+
+// execute
+require('discus');
+require('bootstrap');
+
